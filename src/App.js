1796e050bfd0ca15af5b5b07ca43744829c7718e
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-10-16 15:22:14 +0300

    added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+
+    const [result, setResult] = useState(0);
+
+  function Poga() {
+    setResult(result +1);
+  }
+
+  function Oga() {
+    setResult(result -1);
+  }
+
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <label>Ievadi savu textu: </label>
+      <input type="text" />
+
+      <button onClick={Poga}>+Poga</button> <button onClick={Oga}>-Poga</button>
+
+      <br></br>
+      <br></br>
+
+      <h1>WOW[{result}]</h1>
+
     </div>
   );
 }
+
 
 export default App;
