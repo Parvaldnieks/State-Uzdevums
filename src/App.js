1a3b2d7515c1e562081153977f35cee6a8de932f
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-10-19 09:48:30 +0300

    added

--- src/App.js
+++ src/App.js
@@ -3,24 +3,28 @@ import { useState } from "react";
 function App() {
 
     const [result, setResult] = useState(0);
+    const [ratioChange, setRatioChange] = useState(1);
 
   function Poga() {
-    setResult(result +1);
+    setResult(result + ratioChange);
   }
 
   function Oga() {
-    setResult(result -1);
+    setResult(result - ratioChange);
   }
 
-  
+  function handleRatioChange(event) {
+    setRatioChange(event.target.value);
+  }
 
   return (
     <div className="App">
 
       <label>Ievadi savu textu: </label>
-      <input type="text" />
+      <input type="number" value={ratioChange} onChange={handleRatioChange} />
 
-      <button onClick={Poga}>+Poga</button> <button onClick={Oga}>-Poga</button>
+      <button onClick={Poga}>+Poga</button>
+      <button onClick={Oga}>-Poga</button>
 
       <br></br>
       <br></br>
